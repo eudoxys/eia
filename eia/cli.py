a9d85e90162daf5ff78a6ca6aeb868de1ff2de30
David P. Chassin <david.chassin@me.com>
2025-12-12 15:11:54 -0800

    Fix docs

--- eia/cli.py
+++ eia/cli.py
@@ -28,16 +28,17 @@ def main(*args,**kwargs):
     """
     try:
 
+        # support direct call to main
         if args:
             sys.argv = [__file__] + list(args)
 
+        # setup command line parser
         parser = argparse.ArgumentParser(
             prog="eia",
             description="EIA form data accessor",
             epilog="See https://www.eudoxys.com/eia for documentation. "\
                 "Use `eia help` to get a description of available forms."
             )
-
         parser.add_argument("form",
             choices=[x[4:] for x in globals() if x.startswith("Form")]+["help"])
         parser.add_argument("-o","--output",
@@ -52,11 +53,20 @@ def main(*args,**kwargs):
             help="select states")
         parser.add_argument("--years",
             help="select years")
-        parser.add_argument("--warning",action="store_true")
-        parser.add_argument("--debug",action="store_true")
+        parser.add_argument("--warning",
+            action="store_true",
+            help="enable warning message from python")
+        parser.add_argument("--debug",
+            action="store_true",
+            help="enable debug traceback on exceptions")
+        parser.add_argument("--format",
+            choices=["csv","gzip","zip","xlsx"],
+            help="specify output format")
 
+        # parse arguments
         args = parser.parse_args()
 
+        # setup warning handling
         if not args.warning:
             warnings.showwarning = lambda *x:print(
                 f"WARNING [{__package__}]:",
@@ -65,10 +75,12 @@ def main(*args,**kwargs):
                 file=sys.stderr,
                 )
 
+        # handle help request
         if args.form == "help":
             print(__doc__)
             return E_OK
 
+        # get data
         data = globals()[f"Form{args.form}"](
             years=[int(x) for x in args.years.split(",")] if args.years else None,
             raw=args.raw,
@@ -81,37 +93,37 @@ def main(*args,**kwargs):
                 np.s_[args.states.split(",")] if args.states else np.s_[:],
                 ),:]
 
+        # handle default output
         if args.output is None:
-
             pd.options.display.max_rows = None
             pd.options.display.width = None
             pd.options.display.max_columns = None
             print(data)
             return E_OK
 
-        if args.output.endswith(".csv"):
-
+        # handle CSV output
+        if args.output.endswith(".csv") or args.format == "csv":
             data.to_csv(args.output)
             return E_OK
 
-        if args.output.endswith(".csv.gz"):
-
+        # handle GZIP output
+        if args.output.endswith(".csv.gz") or args.format == "gzip":
             data.to_csv(args.output,compression="gzip")
             return E_OK
 
-        if args.output.endswith(".csv.zip"):
-
+        # handle ZIP output
+        if args.output.endswith(".csv.zip") or args.format == "zip":
             data.to_csv(args.output,compression="zip")
             return E_OK
 
-        if args.output.endswith(".xlsx"):
-
+        # handle XLSX output
+        if args.output.endswith(".xlsx") or args.format == "xlsx":
             data.to_excel(args.output,
                 sheet_name="Form861",
                 merge_cells=False)
             return E_OK
 
-        raise ValueError(f"output format for '{args.output}' is invalid")
+        raise ValueError(f"output format '{args.format}' for '{args.output}' is invalid")
 
     except Exception as err:
 
